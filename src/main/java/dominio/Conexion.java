3bee9c1f1bb86d60c9ee80158a756a23d6d0434e
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2025-06-09 09:17:30 -0300

    Primera subida, estructura general, clase conexion y conector

---
+++ src/main/java/dominio/Conexion.java
@@ -0,0 +1,27 @@
+package dominio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/NUEVONOMBREDBTPINTEGRADOR";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static Connection getSQLConexion() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            conn.setAutoCommit(false);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
