337a7cd3a073150d5fd5da09a167c9aa229e598c
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2025-06-30 14:50:32 -0300

    Agrego script que edita la tabla cliente con el idUsuario relacionado

--- script/edit_tabla_usuario_cliente.sql
+++ script/edit_tabla_usuario_cliente.sql
@@ -13,3 +13,8 @@ DROP COLUMN es_admin;
 --insert usuario ADMIN:
 INSERT INTO usuario (id_cliente, nombre_usuario, contrasena, estado, tipo_usuario)
 VALUES (null, 'admin_user', 'admin123', TRUE, 'admin');
+
+-- EDIT TABLA CLIENTE - Correcciones primera y segunda entrega
+-- cliente tiene FK a usuario
+ALTER TABLE cliente ADD id_usuario INT;
+ALTER TABLE cliente ADD CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario);
